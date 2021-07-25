7204ac05086b1df010e0209a5533e57ae54baece
Jakob Kirsch <jakob.kirsch@web.de>
2021-07-25 02:02:01 +0200

    add data but it"s still broken af

--- limbo/constants.py
+++ limbo/constants.py
@@ -1,14 +1,59 @@
-from pynbt import TAG_Compound
+from pynbt import *
 
-dimension_codec = {
-    "minecraft:dimension_type": TAG_Compound({
-
-    }),
-    "minecraft:worldgen/biome": TAG_Compound({
-
+dimension = {
+    "name": TAG_String("limbo:the"),
+    "id": TAG_Int(0),
+    "element": TAG_Compound({
+        "piglin_safe": TAG_Byte(1),
+        "natural": TAG_Byte(1),
+        "ambient_light": TAG_Float(1.0),
+        "infiniburn": TAG_String(""),
+        "respawn_anchor_works": TAG_Byte(1),
+        "has_skylight": TAG_Byte(1),
+        "bed_works": TAG_Byte(1),
+        "effects": TAG_String("minecraft:the_end"),
+        "has_raids": TAG_Byte(1),
+        "min_y": TAG_Int(0),
+        "height": TAG_Int(256),
+        "logical_height": TAG_Int(256),
+        "coordinate_scale": TAG_Float(1.0),
+        "ultrawarm": TAG_Byte(1),
+        "has_ceiling": TAG_Byte(0)
     })
 }
 
-dimension = {
-    
+biome = {
+    "name": TAG_String("limbo:the_biome"),
+    "id": TAG_Int(0),
+    "element": TAG_Compound({
+        "precipitation": TAG_String("none"),
+        "depth": TAG_Float(0.0),
+        "temperature": TAG_Float(1.5),
+        "scale": TAG_Float(0.0),
+        "downfall": TAG_Float(0.0),
+        "category": TAG_String("none"),
+        "effects": TAG_Compound({
+            "sky_color": TAG_Int(0xfb03ff),
+            "water_fog_color": TAG_Int(0xfb03ff),
+            "fog_color": TAG_Int(0xfb03ff),
+            "water_color": TAG_Int(0xfb03ff),
+        }),
+    })
 }
+
+dimension_codec = {
+    "minecraft:dimension_type": TAG_Compound({
+        "type": TAG_String("minecraft:dimension_type"),
+        "value": TAG_List(TAG_Compound, [
+            TAG_Compound(dimension)
+        ])
+    }),
+    "minecraft:worldgen/biome": TAG_Compound({
+        "type": TAG_String("minecraft:worldgen/biome"),
+        "value": TAG_List(TAG_Compound, [
+            TAG_Compound(biome)
+        ])
+    })
+}
+
+#print(NBTFile(value=dimension_codec).pretty())
